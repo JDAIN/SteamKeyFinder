acb03b1f3023558f6d32de487d8bbf70ce33ccea
JD <poker98@web.de>
2018-11-04 18:06:43 +0100

    deleted bin files and added testcases

--- SteamKeyFinder/src/finder/FinderTest.java
+++ SteamKeyFinder/src/finder/FinderTest.java
@@ -16,9 +16,19 @@ public class FinderTest {
 
   @Test
   public void testFindSimpleKey() {
-    String test = "";
-    Assert.assertEquals("A3G43-23ABW-WE23T", sut.findSteamKeys(test));
-
+    String test = "asda213123hfgh34252jhgj45645456rthhtryh45645rfhgA3G43-23ABW-WE23Tasdadasdasd23421423sfaasdadasdadadadadasdadada";
+    Assert.assertEquals("A3G43-23ABW-WE23T"+" , ", sut.findSteamKeys(test));
   }
 
+  @Test 
+  public void testNoTextNoKey() {
+	  String test ="";
+	  Assert.assertEquals("", sut.findSteamKeys(test));
+  }
+  
+  @Test 
+  public void testNoKey() {
+	  String test ="Halloadasd1 was -asdfsaf-4345345-sdfa234a0s-sfsadf asdf adfsaf sfasf asdfa ";
+	  Assert.assertEquals("", sut.findSteamKeys(test));
+  }
 }
